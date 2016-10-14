1b37a79944e55798397f4bf4db2342adc1582478
Hemant Bansal <hem.ban.061@gmail.com>
2016-10-14 17:51:08 +0530

    Main activity added

--- app/src/main/java/com/example/hemantbansal/woho/LandingActivity.java
+++ app/src/main/java/com/example/hemantbansal/woho/LandingActivity.java
@@ -1,5 +1,6 @@
 package com.example.hemantbansal.woho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,7 +12,7 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-
         strin = "hello";
+        Intent intent ;
     }
 }
