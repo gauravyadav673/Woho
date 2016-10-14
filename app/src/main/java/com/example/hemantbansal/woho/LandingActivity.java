0a801f29c692bc4333eb5f185b0653389b194a6b
gauravyadav673 <gauravrao630@gmail.com>
2016-10-14 17:44:59 +0530

    string added

--- app/src/main/java/com/example/hemantbansal/woho/LandingActivity.java
+++ app/src/main/java/com/example/hemantbansal/woho/LandingActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class LandingActivity extends AppCompatActivity {
 
+    String strin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        strin = "hello";
     }
 }
